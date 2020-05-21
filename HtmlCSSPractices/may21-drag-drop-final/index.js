35e82483e1d4699badf1b6ddf1cdb89e748eca2a
imran <shaikhimran115@gmail.com>
2020-05-22 01:03:31 +0530

    updated drag and drop

--- HtmlCSSPractices/may21-drag-drop-final/index.js
+++ HtmlCSSPractices/may21-drag-drop-final/index.js
@@ -4,12 +4,22 @@ const todoList = document.getElementById('todo');
 let dragItem = null;
 const todos = ['item A', 'item B'];
 console.log(todoList)
-for (let i = 0; i < todos.length; i++) {
-    // const item = listItems[i];
-    // console.log(item);
+
+const input = document.querySelector('.input-box input');
+
+input.addEventListener('keypress', (e) => {
+    let inputVal = e.target.value;
+    if (e.key === 'Enter') {
+        todos.push(inputVal)
+        console.log(inputVal)
+        createOpenList(inputVal);
+        e.target.value = '';
+    }
+});
+const createOpenList = (task) =>{
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('list-item');
-    todoDiv.innerHTML = todos[i];
+    todoDiv.innerHTML = task;
     todoDiv.setAttribute('draggable', true);
     todoList.appendChild(todoDiv);
 
@@ -25,27 +35,29 @@ for (let i = 0; i < todos.length; i++) {
             dragItem.style.display = 'block';
         }, 0);
     })
-    console.log('list => ',lists);
-
-    for (let j = 0; j < lists.length; j++) {
-        const list = lists[j];
-        console.log('list ', list);
-        list.addEventListener('dragover', (e) => {
-            e.preventDefault();
-        })
-
-        list.addEventListener('dragenter', (e) => {
-            e.preventDefault();
-            e.target.backgroundColor = 'rgba(0,0,0,0.2)';
-        })
-
-        list.addEventListener('dragleave', (e) => {
-            e.target.backgroundColor = 'rgba(0,0,0,0.1)';
-        })
-
-        list.addEventListener('drop', (e) => {
-            e.target.appendChild(dragItem);
-            e.target.style.backgroundColor = 'rgba(0,0,0,0.1)';
-        })
-    }
+    console.log('list => ', lists);
 }
+
+
+for (let j = 0; j < lists.length; j++) {
+    const list = lists[j];
+    console.log('list ', list);
+    list.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    })
+
+    list.addEventListener('dragenter', (e) => {
+        e.preventDefault();
+        e.target.backgroundColor = 'rgba(0,0,0,0.2)';
+    })
+
+    list.addEventListener('dragleave', (e) => {
+        e.target.backgroundColor = 'rgba(0,0,0,0.1)';
+    })
+
+    list.addEventListener('drop', (e) => {
+        e.target.appendChild(dragItem);
+        e.target.style.backgroundColor = 'rgba(0,0,0,0.1)';
+    });
+}
+// }
