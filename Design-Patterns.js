076a3158a9734b45ef93013e6ae72aae58fbbbca
Shaikh Imran <shaikhimran115@gmail.com>
2021-04-07 09:21:19 +0530

    Update Design-Patterns.js

--- Design-Patterns.js
+++ Design-Patterns.js
@@ -251,3 +251,247 @@ console.log( basketModule.basket );
 // This also won't work as it only exists within the scope of our
 // basketModule closure, but not in the returned public object
 console.log( basket );
+
+
+-----------------------------------------------------------------------------------------
+ 
+ Singleton Pattern =>
+ =======================
+  The Singleton pattern is thus known because it restricts instantiation of a class to a single object. Classically,
+   the Singleton pattern can be implemented by creating a class with a method that creates a new instance of the class if one doesn't exist.
+In the event of an instance already existing, it simply returns a reference to that object.
+
+Singletons differ from static classes (or objects) as we can delay their initialization, generally because they require some information that may not be
+available during initialization time. They don't provide a way for code that is unaware of a previous reference to them to easily retrieve them.
+This is because it is neither the object or "class" that's returned by a Singleton, it's a structure. Think of how closured variables aren't actually closures
+ - the function scope that provides the closure is the closure.
+
+In JavaScript, Singletons serve as a shared resource namespace which isolate implementation code from the global namespace so as to provide a single point of 
+access for functions.
+
+We can implement a Singleton as follows:
+
+var mySingleton = (function () {
+ 
+  // Instance stores a reference to the Singleton
+  var instance;
+ 
+  function init() {
+ 
+    // Singleton
+ 
+    // Private methods and variables
+    function privateMethod(){
+        console.log( "I am private" );
+    }
+ 
+    var privateVariable = "Im also private";
+ 
+    var privateRandomNumber = Math.random();
+ 
+    return {
+ 
+      // Public methods and variables
+      publicMethod: function () {
+        console.log( "The public can see me!" );
+      },
+ 
+      publicProperty: "I am also public",
+ 
+      getRandomNumber: function() {
+        return privateRandomNumber;
+      }
+ 
+    };
+ 
+  };
+ 
+  return {
+ 
+    // Get the Singleton instance if one exists
+    // or create one if it doesn't
+    getInstance: function () {
+ 
+      if ( !instance ) {
+        instance = init();
+      }
+ 
+      return instance;
+    }
+ 
+  };
+ 
+})();
+ 
+var myBadSingleton = (function () {
+ 
+  // Instance stores a reference to the Singleton
+  var instance;
+ 
+  function init() {
+ 
+    // Singleton
+ 
+    var privateRandomNumber = Math.random();
+ 
+    return {
+ 
+      getRandomNumber: function() {
+        return privateRandomNumber;
+      }
+ 
+    };
+ 
+  };
+ 
+  return {
+ 
+    // Always create a new Singleton instance
+    getInstance: function () {
+ 
+      instance = init();
+ 
+      return instance;
+    }
+ 
+  };
+ 
+})();
+ 
+ 
+// Usage:
+ 
+var singleA = mySingleton.getInstance();
+var singleB = mySingleton.getInstance();
+console.log( singleA.getRandomNumber() === singleB.getRandomNumber() ); // true
+ 
+var badSingleA = myBadSingleton.getInstance();
+var badSingleB = myBadSingleton.getInstance();
+console.log( badSingleA.getRandomNumber() !== badSingleB.getRandomNumber() ); // true
+ 
+// Note: as we are working with random numbers, there is a
+// mathematical possibility both numbers will be the same,
+// however unlikely. The above example should otherwise still
+// be valid.
+
+
+-------------------------------------------------------------------------------------------------
+ 
+ The Prototype Pattern
+ ======================
+  We can think of the prototype pattern as being based on prototypal inheritance where we create objects which act as prototypes for other objects.
+  The prototype object itself is effectively used as a blueprint for each object the constructor creates.
+  If the prototype of the constructor function used contains a property called name for example (as per the code sample lower down),
+   then each object created by that same constructor will also have this same property.
+   
+   var myCar = {
+ 
+  name: "Ford Escort",
+ 
+  drive: function () {
+    console.log( "Weeee. I'm driving!" );
+  },
+ 
+  panic: function () {
+    console.log( "Wait. How do you stop this thing?" );
+  }
+ 
+};
+ 
+// Use Object.create to instantiate a new car
+var yourCar = Object.create( myCar );
+ 
+// Now we can see that one is a prototype of the other
+console.log( yourCar.name );
+
+----
+Object.create also allows us to easily implement advanced concepts such as differential inheritance where objects are able to directly inherit from other objects.
+We saw earlier that Object.create allows us to initialise object properties using the second supplied argument. For example:
+
+
+var vehicle = {
+  getModel: function () {
+    console.log( "The model of this vehicle is.." + this.model );
+  }
+};
+ 
+var car = Object.create(vehicle, {
+ 
+  "id": {
+    value: MY_GLOBAL.nextId(),
+    // writable:false, configurable:false by default
+    enumerable: true
+  },
+ 
+  "model": {
+    value: "Ford",
+    enumerable: true
+  }
+ 
+});
+
+
+--------------------------------------------------------------------------------
+
+
+=> FACAD DESIGN PATTERN =>
+==============================
+ The Façade pattern provides an interface which shields clients from complex functionality in one or more subsystems. 
+ It is a simple pattern that may seem trivial but it is powerful and extremely useful.
+ It is often present in systems that are built around a multi-layer architecture.
+
+The intent of the Façade is to provide a high-level interface (properties and methods) that makes a subsystem or toolkit easy to use for the client.
+
+var Mortgage = function(name) {
+    this.name = name;
+}
+ 
+Mortgage.prototype = {
+ 
+    applyFor: function(amount) {
+        // access multiple subsystems...
+        var result = "approved";
+        if (!new Bank().verify(this.name, amount)) {
+            result = "denied";
+        } else if (!new Credit().get(this.name)) {
+            result = "denied";
+        } else if (!new Background().check(this.name)) {
+            result = "denied";
+        }
+        return this.name + " has been " + result +
+               " for a " + amount + " mortgage";
+    }
+}
+ 
+var Bank = function() {
+    this.verify = function(name, amount) {
+        // complex logic ...
+        return true;
+    }
+}
+ 
+var Credit = function() {
+    this.get = function(name) {
+        // complex logic ...
+        return true;
+    }
+}
+ 
+var Background = function() {
+    this.check = function(name) {
+        // complex logic ...
+        return true;
+    }
+}
+ 
+function run() {
+    var mortgage = new Mortgage("Joan Templeton");
+    var result = mortgage.applyFor("$100,000");
+ 
+    alert(result);
+}
+
+
+--------------------------------------------------------------------------------------------------------------
+
+
