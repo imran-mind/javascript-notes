b92c72d8dbb108bf37914f70f66c36f61d1e827f
imran-mind <shaikhimran115@gmail.com>
2021-07-03 19:54:13 +0530

    'more'

--- JS-Interview-coding-ques/LinkedList/linkedlist.js
+++ JS-Interview-coding-ques/LinkedList/linkedlist.js
@@ -76,6 +76,181 @@ class LinkedList{
         }
         this.size--;
     }
+
+    getFirst(){
+        return this.head.data;
+    }
+
+    getLast(){
+        return this.tail.data;
+    }
+
+    getAtIndex(index){
+        if(this.size === 0)
+            return 'LinkedList is empty';
+        else if(index < 0 || index >= this.size){
+            console.log('Invalid args=>',this.size,index);
+            return -1;
+        }
+        else if(index === 0){
+            // console.log(`Node at Index ${index} ${this.head.data}`);
+            return this.head;
+        }else{
+            let temp = this.head;
+            for(let i=1; i<=index; i++){
+                temp = temp.next;
+            }
+            // console.log(`Node at Index ${index} ${temp.data}`);
+            return temp;
+        }
+    }
+
+    addAt(index,data){
+        if(index < 0 || index >= this.size){
+            console.log('Invalid args=>',this.size,index);
+            return -1;
+        }else if(index === 0){
+            this.addFirst(data);
+        }else{
+            const node = new Node(data);
+            let temp = this.head;
+            for(let i=0; i<index-1; i++){
+                temp = temp.next;
+            }
+            node.next = temp.next;
+            temp.next = node;
+            this.size++;
+        }
+    }
+
+    removeAt(index){
+        if(index < 0 || index >= this.size){
+            console.log('Invalid args=>',this.size,index);
+            return -1;
+        }else if(index === 0){
+            this.removeFirst();
+        }else{
+            let temp = this.head;
+            for(let i=0; i<index-1; i++){
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            // console.log(temp)
+            this.size--;
+        }
+    }
+
+    reverseLinkedList(){
+        if(this.size === 0){
+            console.log('LinkedList is empty')
+        } else if(this.size === 1){
+            return this.head;
+        }else{
+            const list = new LinkedList();
+            const len = this.size;
+            for(let i=0; i<len; i++){
+                const val = this.getFirst();
+                this.removeFirst();
+                list.addFirst(val);
+            }
+            list.display();
+        }
+    }
+
+    
+    reverseLinkedListRecursivly(node){
+        if(node == null){
+            return;
+        }
+        this.reverseLinkedListRecursivly(node.next);
+        console.log(node.data)
+    }
+
+    reverseLinkedListIterativly(){
+        if(this.size === 0){
+            console.log("Linked list is empty");
+        }else if(this.size === 1){
+            return this.head;
+        }else{
+            let li = 0;
+            let ri = this.size - 1;
+            while(li < ri){
+                let left = this.getAtIndex(li);
+                let right = this.getAtIndex(ri);
+                console.log('left',left)
+                let temp = left.data;
+                left.data = right.data;
+                right.data = temp;
+                li++;
+                ri--;
+            }
+        }
+    }
+
+    middleOfLinkedList(){
+        if(this.size === 0){
+            console.log("Linked list is empty");
+        }else if(this.size === 1){
+            return this.head;
+        }else{
+            let slow = this.head;
+            let fast = this.head;
+            while(fast.next != null && fast.next.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow.data;
+        }
+    }
+
+    mergeTwoSortedList(one, two){
+        const list = new LinkedList();
+        while(one != null && two != null){
+            if(one.data < two.data){
+                list.addLast(one.data);
+                one = one.next;
+            }else{
+                list.addLast(two.data);
+                two = two.next;
+            }
+        }
+
+        while(one != null){
+            list.addLast(one.data);
+            one = one.next;
+        }
+        while(two != null){
+            list.addLast(two.data);
+            two = two.next;
+        }
+        list.display()
+    }
+
+    removeDuplicates(){
+        var ptr1 = null, ptr2 = null;
+        ptr1 = this.head;
+ 
+        /* Pick elements one by one */
+        while (ptr1 != null && ptr1.next != null) {
+            ptr2 = ptr1;
+ 
+            /*
+             * Compare the picked element with rest of the elements
+             */
+            while (ptr2.next != null) {
+                 /* If duplicate then delete it */
+                if (ptr1.data == ptr2.next.data) {
+                     /* sequence of steps is important here */
+                    ptr2.next = ptr2.next.next;
+                    this.size--;
+                } else /* This is tricky */ {
+                    ptr2 = ptr2.next;
+                }
+            }
+            ptr1 = ptr1.next;
+        }
+        console.log(this.display())
+    }
 }
 
 const newList = new LinkedList();
@@ -86,7 +261,28 @@ newList.addFirst(10);
 newList.addFirst(20);
 newList.addFirst(30);
 newList.addFirst(40);
+newList.addFirst(30);
+newList.removeDuplicates()
+const newList1 = new LinkedList();
+// newList.addLast(10);
+// newList.addLast(20);
+// newList.addLast(30);
+// newList1.addFirst(60);
+// newList1.addFirst(70);
+// newList1.addFirst(80);
+// newList1.addFirst(90);
+
+// newList.mergeTwoSortedList(newList.head, newList1.head)
+// console.log(newList.middleOfLinkedList());
+// newList.reverseLinkedListRecursivly(newList.head);
+// newList.reverseLinkedListIterativly()
+// newList.reverseLinkedList()
+// newList.removeAt(1)
+// newList.addAt(3,50)
+// newList.getAtIndex(5)
+// console.log(newList.getFirst());
+// console.log(newList.getLast());
 // newList.removeFirst();
-newList.removeLast();
-newList.removeLast();
-newList.display();
+// newList.removeLast();
+// newList.removeLast();
+// newList.display();
