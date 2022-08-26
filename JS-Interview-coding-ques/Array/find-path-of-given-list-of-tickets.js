d02906eb9d0663f20e7f9ac10bc402395b2afbc9
Shaikh Imran <shaikhimran115@gmail.com>
2022-08-26 23:15:37 +0530

    Create find-path-of-given-list-of-tickets.js

---
+++ JS-Interview-coding-ques/Array/find-path-of-given-list-of-tickets.js
@@ -0,0 +1,38 @@
+
+// console.log(findNums(str))
+
+const cityRoutes = (object) =>{
+    const newObj = {};
+    for(let obj in object){
+        console.log(obj);
+        newObj[object[obj]] = obj;
+    }
+    console.log('newObj ',newObj);
+
+    let startPoint = "";
+    for(let obj in object){
+        if(!newObj[obj]){
+            startPoint = obj;
+        }
+    }
+    console.log("SP ",startPoint);
+    let result = [];
+    let newStartPoint = startPoint;
+    for(let obj in object){
+        const arr = [newStartPoint, object[newStartPoint]];
+        newStartPoint = object[newStartPoint];
+        console.log('startPoint -> ',newStartPoint)
+        result.push(arr);
+    }
+    return result;
+}
+
+
+const obj ={
+    "Chennai":"Banglore",
+    "Bombay": "Delhi",
+    "Goa": "Chennai",
+    "Delhi": "Goa"
+}
+
+console.log(cityRoutes(obj));
