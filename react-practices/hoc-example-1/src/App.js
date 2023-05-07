a8147c9b065e697360072da4ed807f4a4de8a5be
imran-mind <shaikhimran115@gmail.com>
2023-05-07 15:38:57 +0530

    added usecallback

--- react-practices/hoc-example-1/src/App.js
+++ react-practices/hoc-example-1/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Parent from './components/Parent';
 
+//https://blog.logrocket.com/understanding-react-higher-order-components/#:~:text=According%20to%20React's%20documentation%2C%20a,const%20newComponent%20%3D%20higherFunction(WrappedComponent)%3B
 function App() {
   return (
     <div className="App">
