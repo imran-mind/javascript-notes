ffc06057ff18ebf30660d9bec40dbd3ea23b6b20
imran-mind <shaikhimran115@gmail.com>
2023-05-29 12:04:14 +0530

    improved groupBy polyfills

--- JS-Polyfills/groupBy.js
+++ JS-Polyfills/groupBy.js
@@ -1,16 +1,38 @@
 
-const groupBy = (collection, iterator)=>{
-    const obj = {};
-    if(iterator === 'length' && collection.length){
-        collection.forEach((item)=>{
-            const n = item.length;
-            if(obj[n]){
-                obj[n].push(item);
-            }else{
-                obj[n] = [item];
-            }
-        })
+const groupBy = (values, keyFinder)=>{
+   //using reduce to aggregate value
+   return values.reduce((a,b)=>{
+    //depending upon the type of keyFind[string or func]
+    //if it's a fucn pass the value to it
+    //if it's a property, access the property
+    const key = typeof keyFinder === 'function' ? keyFinder(b) : b[keyFinder];
+    if(!a[key]){
+        a[key] = [b];
+    }else{
+        a[key] = [...a[key], b];
     }
+    return a;
+   },{})
+}
+
+// my approach
+const _groupBy = (arr, finder)=>{
+    const obj = {};
+    arr.forEach((item)=>{
+        const key = typeof finder === 'function' ? finder(item) : item[finder];
+        if(obj[key]){
+            obj[key].push(item);
+        } else{
+            obj[key] = [item];
+        }
+    })
     return obj;
 }
+console.log(groupBy([6.1, 4.2, 6.3], Math.floor)); 
 console.log(groupBy(['one', 'two', 'three'], 'length'));
+
+console.log(_groupBy([6.1, 4.2, 6.3], Math.floor)); 
+console.log(_groupBy(['one', 'two', 'three'], 'length'));
+// Output:
+// { 6: [6.1, 6.3], 4: [4.2] }
+// { 3: ['one', 'two'], 5: ['three'] }
